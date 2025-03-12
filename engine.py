e9c54d0dd5315baf96df25f871d2ce3fe5a56c62
SimerusM <63352151+SimerusM@users.noreply.github.com>
2025-03-12 08:51:51 -0400

    backpropogation logic to engine

--- engine.py
+++ engine.py
@@ -43,3 +43,18 @@ class Value:
         
         out._backward = _backward
         return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
