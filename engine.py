bb46231556e5f68b4843ea51cb768718a56135e1
SimerusM <63352151+SimerusM@users.noreply.github.com>
2025-03-26 10:17:47 -0400

    added more operations and scalar type checking for add/mul

--- engine.py
+++ engine.py
@@ -13,6 +13,7 @@ class Value:
         return f"Value(data={self.data}, op={self._op})"
     
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other) # turn scalars into a Value object
         out = Value(self.data + other.data, (self, other), '+')
         
         def _backward(): # Create closure for it
@@ -22,7 +23,11 @@ class Value:
         out._backward = _backward
         return out
     
+    def __radd__(self, other): # other + self, in case self + other fails
+        return self * other
+    
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other) # turn scalars into a Value object
         out = Value(self.data * other.data, (self, other), '*')
         
         def _backward(): # Create closure for it
@@ -30,6 +35,39 @@ class Value:
             other.grad += self.data * out.grad
         
         out._backward = _backward
+        return out
+    
+    def __rmul__(self, other): # other * self, in case self * other fails
+        return self * other
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supports int/float"
+        out = Value(self.data**other, (self,), f'**{other}')
+        
+        def _backward():
+            self.grad += other * self.data**(other - 1) * out.grad
+            
+        out._backward = _backward
+        return out
+    
+    def __truediv__(self, other): 
+        return self * other**-1 # calls __mul__
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other) # calls __add__
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+        
+        def _backward():
+            self.grad += out.data * out.grad
+            
+        out._backward = _backward
+        
         return out
     
     def tanh(self):
@@ -43,7 +81,7 @@ class Value:
         
         out._backward = _backward
         return out
-
+    
     def backward(self):
         topo = []
         visited = set()
